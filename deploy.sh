c7f3cc8220b882642ff2584a863d247e4c6a01ba
sogrey <suzhaoheng2006@163.com>
2019-09-19 09:46:20 +0800

    deploy master

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,13 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-echo -e "\033[32;40m [1/5] \033[0m init..."
+echo -e "\033[32;40m [1/4] \033[0m init..."
 
 npm install
 npm install -g vuepress
 npm audit fix
-pip install requests 
-pip install beautifulsoup4 
-pip install pdfkit
 
-echo -e "\033[32;40m [2/5] \033[0m create pdf..."
-
-cd vuepress2pdf 
-python vuepress2pdf.py
-
-echo -e "\033[32;40m [3/5] \033[0m commit 2 master branch"
-
-cd .. 
+echo -e "\033[32;40m [2/4] \033[0m commit 2 master branch"
 
 git init
 git add -A
@@ -28,11 +18,11 @@ git commit -m 'deploy master'
 # 如果你想要部署到 https://USERNAME.github.io
 git push -f https://github.com/Sogrey/VuepressBlogTemplate.git master
 
-echo -e "\033[32;40m [4/5] \033[0m Building static files"
+echo -e "\033[32;40m [3/4] \033[0m Building static files"
 # 生成静态文件
 vuepress build source --dest docs
 
-echo -e "\033[32;40m [5/5] \033[0m commit 2 gh-pages branch"
+echo -e "\033[32;40m [4/4] \033[0m commit 2 gh-pages branch"
 # 进入生成的文件夹
 cd docs/
 # 如果是发布到自定义域名
@@ -43,5 +33,7 @@ git add -A
 git commit -m 'deploy gh-pages'
 
 git push -f https://github.com/Sogrey/VuepressBlogTemplate.git master:gh-pages
+
+echo -e "\033[32;40m done \033[0m "
 
 cd -
